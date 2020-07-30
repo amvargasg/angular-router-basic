27c084a5e3b961cc0ddb0a8d2ef48b84f3fb4c00
amvargasg <am.vargasg@uniandes.edu.co>
2020-07-30 18:46:41 -0500

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,16 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {
-  MatButtonModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule
-} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -21,22 +12,7 @@ import { AppRoutingModule } from './app.routing.module';
 /**
  * The modules for angular material
  */
-const MATERIAL_MODULES = [
-  MatButtonModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-]
 
-@NgModule({
-  exports: [MATERIAL_MODULES],
-  imports: [MATERIAL_MODULES]
-})
-export class MaterialModule { }
 
 
 @NgModule({
